8d0d4c52c1d30bbb4b846ea9f09c6504e9972160
Skanda2007 <82853099+Skanda2007@users.noreply.github.com>
2021-07-26 14:48:32 +0530

    Update Simple.c

--- Simple.c
+++ Simple.c
@@ -7,10 +7,11 @@ int main()
     int get;
     printf("Enter the number of Loops >>");
     scanf("%d", &get);
+    printf("0\n");
     for(int i=1;i<=get;i++)
     {
         ans=n1+n2;
-        printf("%d=%d+%d\n", ans, n1, n2);
+        printf("%d\n", ans);
         n2=n1;
         n1=ans;
     }
